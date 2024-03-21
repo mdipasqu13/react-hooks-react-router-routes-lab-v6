442bdb139a8bc31f02316b216b026f35f94b8f30
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-03-21 15:01:03 -0400

    just getting started

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
+const router = createBrowserRouter([
+    {
+        path: "/"
+        element: <Home/>
+    },
+    {
+        path:"/movie"
+        element: <Movie/>
+    },
+    {
+        path:"directors"
+        element: <Directors/>
+    },
+    {
+        path:"actos"
+        element: <Directors/>
+    },
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar"
+import MovieCard from "../components/MovieCard";
 
 function Home() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
       </main>
     </>
   );
